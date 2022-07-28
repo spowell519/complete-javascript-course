0ac0d85e68b04ac1adff312742a77bcac5df7053
Stephanie Powell <spowell519@gmail.com>
2022-07-28 17:28:30 -0400

    Initial Commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+// document.querySelector(".message").textContent = "Number is correct!🥳 "
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".number").textContent = 19;
+// document.querySelector(".score").textContent = 9;
+
+// document.querySelector(".guess").value = 4;
+// console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener("click", function() {
+    const guess = Number(document.querySelector(".guess").value);
+ console.log(guess,typeof guess);
+
+ if (!guess) {
+    document.querySelector(".message").textContent = 
+    "🙅‍♀️ No number given!";
+ }
+});
