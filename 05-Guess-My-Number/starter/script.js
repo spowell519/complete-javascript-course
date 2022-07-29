06e1dd9cffd3b8a50c31aa3512ab31ddad23010e
Stephanie Powell <spowell519@gmail.com>
2022-07-29 11:28:40 -0400

    Score and message updates

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // document.querySelector(".message").textContent = "Number is correct!🥳 "
 // console.log(document.querySelector(".message").textContent);
 // document.querySelector(".number").textContent = 19;
@@ -8,13 +7,35 @@
 
 // document.querySelector(".guess").value = 4;
 // console.log(document.querySelector(".guess").value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector(".check").addEventListener("click", function() {
-    const guess = Number(document.querySelector(".guess").value);
- console.log(guess,typeof guess);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
- if (!guess) {
-    document.querySelector(".message").textContent = 
-    "🙅‍♀️ No number given!";
- }
+  if (!guess) {
+    document.querySelector('.message').textContent = '🙅‍♀️ No number given!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Number is correct!🥳 ';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high ⬆️';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😭';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low ⬇️';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😭';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
