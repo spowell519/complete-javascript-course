bb35c2af48803019291cf9e030b399fb65e2fa9e
Stephanie Powell <spowell519@gmail.com>
2022-08-04 11:46:27 -0400

    Adding functionality to the Escape button

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,11 +6,11 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-const openModal = function() {
-    console.log('Button Clicked!');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+const openModal = function () {
+  console.log('Button Clicked!');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -26,3 +26,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 /* Software developers often use classes to style web applications
 You can add/remove specific classes.  We created the closeModal/openModal functions to prevent redundancy and keep pur code DRY!
 */
+document.addEventListener('keydown', function (event) {
+  //   console.log('A key was pressed!');
+  console.log(event.key);
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
